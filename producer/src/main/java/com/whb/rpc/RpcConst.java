20e0c8a27c2386da176e178fd48d1214ea209b28
whbjiushiwo123 <jiushini0908@126.com>
2021-08-13 20:25:51 +0800

    rabbitmq:使用rpc订单

---
+++ producer/src/main/java/com/whb/rpc/RpcConst.java
@@ -0,0 +1,11 @@
+package com.whb.rpc;
+
+/**
+ */
+public class RpcConst {
+
+    public static final String SUCCESS = "success";
+    public static final String FAILURE = "failure";
+    public static final String EXCEPTION = "exception";
+
+}
