12bde229c8bb802930dc64779ecafb684a25c1f1
whbjiushiwo123 <jiushini0908@126.com>
2021-08-23 21:33:03 +0800

    rabbitmq:模拟下单

---
+++ consumer/src/main/java/com/whb/vo/GoodTransferVo.java
@@ -0,0 +1,33 @@
+package com.whb.vo;
+
+import java.io.Serializable;
+
+public class GoodTransferVo implements Serializable {
+    private String goodsId;
+    private int changeAmount;
+    private boolean inOrOut;
+
+    public String getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(String goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public int getChangeAmount() {
+        return changeAmount;
+    }
+
+    public void setChangeAmount(int changeAmount) {
+        this.changeAmount = changeAmount;
+    }
+
+    public boolean isInOrOut() {
+        return inOrOut;
+    }
+
+    public void setInOrOut(boolean inOrOut) {
+        this.inOrOut = inOrOut;
+    }
+}
